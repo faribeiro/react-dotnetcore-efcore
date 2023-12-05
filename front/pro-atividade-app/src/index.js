01c7e0aa84383da920c2dddc7c4b828f84531bc1
faribeiro <fb.ribeiro@hotmail.com>
2023-12-05 16:27:02 -0300

    Seção 06 - React - Organização e Rotas

--- front/pro-atividade-app/src/index.js
+++ front/pro-atividade-app/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import Menu from './components/Menu';
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className='container'>
-    <App />
-    
-  </div>
+  <Router> 
+    <Menu/>
+    <div className='container'>
+      <App />    
+    </div>
+  </Router>, 
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ front/pro-atividade-app/src/pages/clientes/ClienteLista.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
+import TitlePage from "../../components/TitlePage";
+
+const clientes = [
+  {
+    id: 1,
+    nome: "Microsoft",
+    responsavel: "Otto",
+    contato: "1055751",
+    situacao: "Ativo",
+  },
+
+  {
+    id: 2,
+    nome: "Amazon",
+    responsavel: "Luke",
+    contato: "1055521",
+    situacao: "Ativo",
+  },
+
+  {
+    id: 3,
+    nome: "Google",
+    responsavel: "Frank",
+    contato: "1025549",
+    situacao: "Ativo",
+  },
+
+  {
+    id: 4,
+    nome: "Facebook",
+    responsavel: "Karl",
+    contato: "1054190",
+    situacao: "Ativo",
+  },
+
+  {
+    id: 5,
+    nome: "Twitter",
+    responsavel: "Jack",
+    contato: "1055580",
+    situacao: "Ativo",
+  },
+];
+
+export default function ClienteLista() {
+  const history = useHistory();
+  const [termoBusca, setTermoBusca] = useState('');
+  
+  const handleInputChange = (e) => {
+      setTermoBusca(e.target.value);
+  }
+
+  const clientesFiltrados = clientes.filter((cliente) => {
+      return Object.values(cliente)
+        .join(' ')
+        .toLowerCase()
+        .includes(termoBusca.toLowerCase());
+  }) ;
+
+  const novoCliente = () => {
+      history.push('/cliente/detalhe')
+  }
+   
+  return (
+    <>
+      <TitlePage title="Cliente Lista" >
+        <Button variant='outline-secondary' onClick={novoCliente}>
+          <i className='fas fa-plus me-2'></i>
+          Novo Cliente
+        </Button>
+      </TitlePage>
+      <InputGroup className="mt-3 mb-3">
+        <InputGroup.Text>
+          Buscar:
+        </InputGroup.Text>
+        <FormControl
+          onChange={handleInputChange}
+          placeholder='Buscar por nome do cliente'
+        />
+      </InputGroup>
+      <table className="table dark-striped table-hover">
+        <thead className="table-dark mt-3">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Responsável</th>
+            <th scope="col">Contato</th>
+            <th scope="col">Situação</th>
+            <th scope="col">Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientesFiltrados.map((cliente) => (
+            <tr key={cliente.id}>
+              <td>{cliente.id}</td>
+              <td>{cliente.nome}</td>
+              <td>{cliente.responsavel}</td>
+              <td>{cliente.contato}</td>
+              <td>{cliente.situacao}</td>
+              <td>
+                <div>
+                  <button className="btn btn-sm btn-outline-primary me-2" onClick={() => history.push(
+                    `/cliente/detalhe/${cliente.id}`
+                  )}>
+                    <i className="fas fa-user-edit me-2"></i>
+                    Editar
+                  </button>
+                  <button className="btn btn-sm btn-outline-danger me-2">
+                    <i className="fas fa-user-times me-2"></i>
+                    Desativar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
