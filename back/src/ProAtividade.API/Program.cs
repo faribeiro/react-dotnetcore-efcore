c80163cea6aeab7f6171d7e390f8221c83212e84
faribeiro <fb.ribeiro@hotmail.com>
2023-12-12 15:34:26 -0300

    Seção 07 - .NET 5 para .NET 6 ( já estava no .NET7

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,24 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using ProAtividade.Domain.Interfaces.Repositories;
-using ProAtividade.Data.Repositories;
-using ProAtividade.Domain.Interfaces.Services;
-using ProAtividade.Domain.Services;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
